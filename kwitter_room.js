504445c84b2c8cbf1d1e06d40aaecb8cba4fed4d
Dhriti2010 <89203702+Dhriti2010@users.noreply.github.com>
2022-03-30 16:40:27 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,13 +1,13 @@
 
 //ADD YOUR FIREBASE LINKS HERE
 var firebaseConfig = {
-      apiKey: "AIzaSyDiYTYy8IjdGmYogGngnkIMmxre31iftFw",
-      authDomain: "kwitter-11c6f.firebaseapp.com",
-      databaseURL: "https://kwitter-11c6f-default-rtdb.firebaseio.com",
-      projectId: "kwitter-11c6f",
-      storageBucket: "kwitter-11c6f.appspot.com",
-      messagingSenderId: "586266378365",
-      appId: "1:586266378365:web:51685515f3fd88f941f114"
+      apiKey: "AIzaSyDods5cVkbT0zBg9HfgahpZhMXCAP45W9M",
+      authDomain: "let-s-chat-14876.firebaseapp.com",
+      databaseURL: "https://let-s-chat-14876-default-rtdb.firebaseio.com",
+      projectId: "let-s-chat-14876",
+      storageBucket: "let-s-chat-14876.appspot.com",
+      messagingSenderId: "214078714720",
+      appId: "1:214078714720:web:b539bca0f32d82a437a405"
     };
     
     // Initialize Firebase
@@ -25,10 +25,17 @@ var firebaseConfig = {
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
-      //End code
-      });});}
-getData();
+      row = "<div class='room_name' id="+Room_names+" onclick='redirecttoroomname(this.id)' >#"+ Room_names +"</div><hr>";
+      document.getElementById("output").innerHTML+=row;
+            //End code
+            });});}
+      getData();
+      function redirecttoroomname(name){
+            console.log(name);
+            localStorage.setItem("roomnname",name);
+            window.location="kwitter_page.html"
+      }
+     
 function logout(){
       localStorage.removeItem("username");
       localStorage.removeItem("roomname");window.location="index.html";
